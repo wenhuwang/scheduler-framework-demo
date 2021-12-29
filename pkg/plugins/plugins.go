c5892d63a5322589429b94483d65bffb40806297
wang_wenhu <976400757@qq.com>
2021-12-29 17:48:09 +0800

    init v1.19.16 version.

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -2,65 +2,75 @@ package plugins
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/klog"
-	framework "k8s.io/kubernetes/pkg/scheduler/framework"
-	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
+	"k8s.io/klog/v2"
+	frameworkruntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
+	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
 )
 
 // 插件名称
-const Name = "sample-plugin"
+const Name = "RealResourceUsage"
+
+type Args struct {
+	FavoriteColor  string `json:"favorite_color,omitempty"`
+	FavoriteNumber int    `json:"favorite_number,omitempty"`
+	ThanksTo       string `json:"thanks_to,omitempty"`
+}
 
 type Sample struct {
-	kubeClient    *kubernetes.Clientset
-	metricsClient *metricsv.Clientset
-	args          *NodeResourcesUsageArgs
-	handle        framework.Handle
+	args   *Args
+	handle framework.FrameworkHandle
 }
+
+var _ framework.FilterPlugin = &Sample{}
+var _ framework.ScorePlugin = &Sample{}
 
 func (s *Sample) Name() string {
 	return Name
 }
 
-func (s *Sample) PreFilter(ctx context.Context, pod *v1.Pod) *framework.Status {
-	klog.V(3).Infof("prefilter pod: %v", pod.Name)
+// func (s *Sample) PreFilter(ctx context.Context, pod *v1.Pod) *framework.Status {
+// 	klog.V(3).Infof("prefilter pod: %v", pod.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
+
+func (s *Sample) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	// klog.V(10).Infof("filter pod: %v, node: %v", pod.Name, nodeInfo.Node().Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
-func (s *Sample) Filter(ctx context.Context, pod *v1.Pod, nodeName string) *framework.Status {
-	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName)
-	// if nodeName == "shtl009063227" {
-	// 	return framework.NewStatus(framework.Unschedulable, "")
-	// }
-	return framework.NewStatus(framework.Success, "")
-}
-
-func (s *Sample) Score(ctx context.Context, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
-	klog.V(3).Infof("Score pod: %v, node: %v", pod.Name, nodeName)
+func (s *Sample) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
+	// klog.V(10).Infof("Score pod: %v, node: %v", pod.Name, nodeName)
 	nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
 	if err != nil {
-		return framework.MinNodeScore, framework.NewStatus(framework.Error, "")
+		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("getting node %q from Snapshot: %v", nodeName, err))
 	}
 
-	nodeMetrics, err := s.metricsClient.MetricsV1beta1().NodeMetricses().Get(ctx, nodeName, metav1.GetOptions{})
-	if err != nil {
-		klog.Errorf("get node %s metrics err: %+v", nodeName, err)
+	metricsString, ok := nodeInfo.Node().Annotations[metricsAnnotation]
+	if !ok {
+		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("node %s annotations %s is not exists.", nodeName, metricsAnnotation))
 	}
 
-	// Calculation formula Details:
-	// (cpu((100-Usage*100)/10) + memory((100-usage*100)/10))/2
-	cpuMetrics := float64(100*nodeMetrics.Usage.Cpu().MilliValue()) / float64(nodeInfo.Allocatable.MilliCPU)
-	memMetrics := float64(100*nodeMetrics.Usage.Memory().Value()) / float64(nodeInfo.Allocatable.Memory)
-	klog.V(3).Infof("node name: %s, cpu metrics %f, mem metrics %f", nodeInfo.Node().Name, cpuMetrics, memMetrics)
+	metrics := make(map[string]float64)
+	if err = json.Unmarshal([]byte(metricsString), &metrics); err != nil {
+		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("node %s annotations %s parse failed. %v", nodeName,
+			metricsAnnotation, err))
+	}
+	cpuMetrics := metrics[cpuMetricsKey] * 100
+	nodeScore := int64(100 - cpuMetrics)
 
-	nodeScore := ((100-cpuMetrics)/10 + (100-memMetrics)/10) / 2
-	return int64(nodeScore), framework.NewStatus(framework.Success, "")
+	if klog.V(10).Enabled() {
+		klog.Infof("%v -> %v: %v, score %d", pod.Name, nodeName, Name, nodeScore)
+	}
+	return nodeScore, framework.NewStatus(framework.Success, "")
+}
+
+func (s *Sample) ScoreExtensions() framework.ScoreExtensions {
+	return nil
 }
 
 func (s *Sample) NormalizeScore(ctx context.Context, pod *v1.Pod, nodeScoreList framework.NodeScoreList) *framework.Status {
@@ -77,40 +87,15 @@ func (s *Sample) NormalizeScore(ctx context.Context, pod *v1.Pod, nodeScoreList 
 // 	}
 // }
 
-func getargs(obj runtime.Object) (*NodeResourcesUsageArgs, error) {
-	args, ok := obj.(*NodeResourcesUsageArgs)
-	if !ok {
-		return &NodeResourcesUsageArgs{}, fmt.Errorf("want args to be of type Args, got %T", obj)
-	}
-	return args, nil
-}
-
-func New(configuration runtime.Object, f framework.Handle) (framework.Plugin, error) {
-	args, err := getargs(configuration)
-	if err != nil {
+func New(configuration runtime.Object, f framework.FrameworkHandle) (framework.Plugin, error) {
+	args := &Args{}
+	if err := frameworkruntime.DecodeInto(configuration, args); err != nil {
 		return nil, err
 	}
 	klog.V(3).Infof("get plugin config args: %+v", args)
 
-	config, err := clientcmd.BuildConfigFromFlags("", args.KubeConfig)
-	if err != nil {
-		klog.Errorf("get config err: %+v", err)
-	}
-
-	kubeClient, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		klog.Errorf("get kubeClient err: %+v", err)
-	}
-
-	metricsClient, err := metricsv.NewForConfig(config)
-	if err != nil {
-		klog.Errorf("get metricsClient err: %+v", err)
-	}
-
 	return &Sample{
-		kubeClient:    kubeClient,
-		metricsClient: metricsClient,
-		args:          args,
-		handle:        f,
+		args:   args,
+		handle: f,
 	}, nil
 }
