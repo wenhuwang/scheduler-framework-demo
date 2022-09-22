1122e813157f29e16e2ee68b738b70fa63973bf1
wenhuwang <wang15691700816@gmail.com>
2022-09-22 17:37:50 +0800

    - Score: fix the case where the node exception caused the CPU utilization to be 0 and the node got a high score - Filter: support filtering high-load nodes, egs: CPU, memory, load1, load5

--- pkg/plugins/constants.go
+++ pkg/plugins/constants.go
@@ -3,4 +3,7 @@ package plugins
 const (
 	metricsAnnotation = "metrics.kubernetes.io"
 	cpuMetricsKey     = "node_cpu_utilisation"
+	memMetricsKey     = "node_memory_utilisation"
+	load1MetricsKey   = "node_load1"
+	load5MetricsKey   = "node_load5"
 )
